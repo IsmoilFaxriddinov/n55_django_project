36fbc222fead37bc1d73f55c9e1db682ec365501
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-30 18:54:34 +0500

    a

--- blogs/views.py
+++ blogs/views.py
@@ -10,10 +10,10 @@ def blog_page_view(request):
     return render(request, 'blogs/blog_list.html', context)
 
 def blog_detail_view(request, pk):
-    blog = BlogModel.objects.get(id=pk)
+    blog = BlogModel.objects.filter(id=pk).first()
     if blog is not None:
         context = {
-            'blog': blog
+            'blog': blog.first()
         }
         return render(request, 'blogs/blog_detail.html', context)
     else:
