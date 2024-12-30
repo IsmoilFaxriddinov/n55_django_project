8f56e4b75bd694f1665ce8465adfdd6248357ffc
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-30 20:06:26 +0500

    a

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from shop.models import ProductModel
+
+product = ProductModel.objects.all()
+context = {
+    'product': product
+}
 
 def product_list_view(request):
-    return render(request, 'shop/product-list.html')
+    return render(request, 'shop/product-list.html', context)
