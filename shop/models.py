9b03cdeb2b8c488b451c4c1a47cf2dc24c513ed9
Ismoil <ismoilfaxriddinov2010@gmail.com>
2024-12-31 16:58:37 +0500

    a

--- shop/models.py
+++ shop/models.py
@@ -4,6 +4,8 @@ class ProductModel(models.Model):
     img = models.ImageField(upload_to='shop')
     name = models.CharField(max_length=50)
     price = models.FloatField()
+    discount = models.FloatField()
+    description = models.TextField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
