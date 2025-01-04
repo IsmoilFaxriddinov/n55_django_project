5e0920e94d84ff8c532e066e6ab9b8ddebb06af5
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-04 14:49:03 +0500

    a

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from pages.forms import ContactForm
 
 def home_page_view(request):
     return render(request, 'index.html')
@@ -7,4 +8,7 @@ def about_page_view(request):
     return render(request, 'pages/about.html')
 
 def contact_page_view(request):
-    return render(request, 'pages/contact.html')
+    if request.method == 'GET':
+        return render(request, 'pages/contact.html')
+    elif request.method == "POST":
+        forms = ContactForm(request.POST)
