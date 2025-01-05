3b049dc9acfd6af70a51dd436b3d4254a751b4aa
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-05 22:10:21 +0500

    a

--- blogs/models.py
+++ blogs/models.py
@@ -1,44 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# UserModel = get_user_model()
-
-# class ProfileModel(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     avatar = models.ImageField(upload_to='avatars', null=True, blank=True)
-#     about = models.CharField(max_length=255, null=True, blank=True)
-    
-#     def __repr__(self):
-#         return self.user.get_full_name()
-    
-#     def __str__(self):
-#         return self.user.get_full_name()
-    
-#     class Meta:
-#         verbose_name = 'Profile'
-#         verbose_name_plural = 'Profiles'
+from pages.models import BaseModel
 
 
-class BlogHashganModel(models.Model):
+class BlogCategoryModel(BaseModel):
     title = models.CharField(max_length=128)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
 
     class Meta:
         verbose_name = 'blog hashtag'
         verbose_name_plural = 'blog hashtags'
 
-class BlogModel(models.Model):
+class BlogHashganModel(BaseModel):
+    title = models.CharField(max_length=128)
+
+    class Meta:
+        verbose_name = 'blog hashtag'
+        verbose_name_plural = 'blog hashtags'
+
+class BlogModel(BaseModel):
     author = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='blogs', null=True, blank=True)
     image = models.ImageField(upload_to='blogs')
     title = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     long_description = models.TextField()
     hashtags = models.ManyToManyField(BlogHashganModel, related_name='blogs')
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.title
--- pages/models.py
+++ pages/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 
-class ContactModel(models.Model):
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class ContactModel(BaseModel):
     name = models.CharField(max_length=125)
     email = models.EmailField()
     subject = models.CharField(max_length=255)
     text = models.TextField()
 
-    created_ad = models.DateTimeField(auto_now_add=True)
-    updated_ad = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
