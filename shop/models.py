81a8c5080d4f39f44cdd622d9b432beac420917e
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-09 14:48:44 +0500

    a

--- shop/models.py
+++ shop/models.py
@@ -1,14 +1,13 @@
 from django.db import models
 
-class ProductModel(models.Model):
+from pages.models import BaseModel
+
+class ProductModel(BaseModel):
     img = models.ImageField(upload_to='shop')
     name = models.CharField(max_length=50)
     price = models.FloatField()
     discount = models.FloatField()
     description = models.TextField()
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
         return self.name
