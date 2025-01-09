9a546765268cf21e87a1f9d543c7c6e87e8e8794
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-09 23:42:25 +0500

    a

---
+++ users/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+UserModel = get_user_model()
+
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = UserModel
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
+
+class LoginForm(forms.Form)
+    email_or_username = forms.CharField(max_length=125)
+    password = forms.CharField(max_length=125)
+    
--- users/views.py
+++ users/views.py
@@ -1,4 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from users.forms import RegisterForm
+from users.models import CustomUserModel
 
 def register_view(request):
-    return render(request, 'auth/register.html')
+    if request.method == "GET":
+        return render(request, 'auth/register.html')
+    elif request.method == "POST":
+        data = request.POST
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.is_active = False
+            user.set_password(raw_password=data['password1'])
+            user.save()
+            messages.success(request, 'Please, confirm your email or login')
+        return redirect('users:login')
