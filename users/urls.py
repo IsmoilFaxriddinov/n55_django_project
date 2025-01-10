edc514e3b8660e13fb45e7bfc98f248d200a41b3
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-10 19:11:45 +0500

    a

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 
-from users.views import confirm_email, register_view
+from users.views import confirm_email, login_view, register_view
 
 app_name = 'users'
 
 urlpatterns = [
     path('register/', register_view, name="register"),
-    path('login/', register_view, name="login"),
+    path('login/', login_view, name="login"),
     path('confirm/email/<int:uid>/<str:token>/', confirm_email, name="confirm_email")
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
 import threading
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 from django.shortcuts import redirect, render
+from django.db.models import Q
 from django.contrib import messages
-from users.forms import RegisterForm
+from users.forms import LoginForm, RegisterForm
 from users.models import CustomUserModel
 from users.utils import send_email_confirmation
 from users.utils import default_token_generator
@@ -42,3 +45,26 @@ def confirm_email(request, uid, token):
     else:
         messages.success(request, 'Link is not correnct')
         return redirect('users:login')
+    
+def login_view(request):
+    if request.method == "GET":
+        return render(request, 'auth/login.html')
+    elif request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid:
+            email_or_username = request.POST['email_or_username']
+            password = request.POST['password']
+            try:
+                user = CustomUserModel.objects.get(Q(username=email_or_username) | Q(email=email_or_username))
+            except CustomUserModel.DoesNotExist:
+                return redirect('users:login')
+            
+            authenticated_user = authenticate(user=user, password=password)
+            if authenticated_user is not None:
+                login(request, authenticated_user)
+                return redirect('pages:home')
+            else:
+                return redirect('users:login')
+        else:
+            return redirect('users:login')
+                
