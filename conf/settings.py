5ba36e2191b323b4ae24dc69488400d4e47ec099
Ismoil <ismoilfaxriddinov2010@gmail.com>
2025-01-10 17:23:26 +0500

    a

--- conf/settings.py
+++ conf/settings.py
@@ -143,5 +143,6 @@ AUTH_USER_MODEL = 'users.CustomUserModel'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Yoki boshqa provayder
 EMAIL_PORT = 587
+EMAIL_USE_TLS = True    
 EMAIL_HOST_USER = 'ismoilfaxriddinov2010@gmail.com'
 EMAIL_HOST_PASSWORD = 'zqlg yllb swzi pwyk'
